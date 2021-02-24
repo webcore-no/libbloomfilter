a7a2754d6c033ba93c74677d0ae0b8e97e17d269
Odin Hultgren Van Der Horst <odin@digitalgarden.no>
2021-02-24 12:56:44 +0100

    Added lua FFI and some readme

--- README.md
+++ README.md
@@ -1,3 +1,58 @@
-# Libbloomfilter - A lockless bloom filter immplemented in c
+WIP
+# Libbloomfilter - A lock-less bloom filter implemented in c
+
 Uses xxh3 for hashing
 Uses 2 keys on compile time sized defined table
+
+## Usage
+### Normal
+```c
+```
+### Swap
+
+#### Construction
+``bloomfilterswap_new(allocator)`` Create a new swaping bloomfilter
+
+``bloomfilterswap_destroy(filter, deallocator)`` Destroy the filter
+#### Operations
+``bloomfilterswap_swap(filter)``  Changes active filter, and clears the background filter
+
+``bloomfilterswap_add(filter, key, keylen)`` adds key to background and active filter
+
+``bloomfilterswap_test(filter, key, keylen)`` Checks active filter for key
+
+#### LUA
+```openresty
+worker_processes  12;
+
+events{}
+
+http {
+    init_by_lua_block {
+        local err
+        bloom, err = require("bloomfilter")()
+        if not bloom then
+            return ngx.log(ngx.ERR, "INIT_ERR: ", err)
+        end
+        _G.bloom = bloom
+    }
+
+    server {
+        listen 8080;
+        location / {
+            content_by_lua_block {
+                if ngx.var.uri == "/swap" then
+                    bloom.swap()
+                    return ngx.say("SWAP");
+                end
+                if ngx.var.request_method == "POST" then
+                    bloom.add(ngx.var.uri);
+                    return ngx.say("OK")
+                end
+                ngx.log(ngx.ERR, bloom.test(ngx.var.uri));
+                return bloom.test(ngx.var.uri) and ngx.say("HIT") or ngx.exit(404)
+            }
+        }
+    }
+}
+```
---
+++ lua/bloomfilter.lua
@@ -0,0 +1,66 @@
+local ffi = require("ffi")
+
+ffi.cdef[[
+typedef struct bloomfilter_swap_s bloomfilter_swap_t;
+typedef void *(*bloomfilter_allocator)(size_t);
+typedef void (*bloomfilter_deallocator)(void *, size_t);
+
+void *bloomfilter_shm_alloc(size_t);
+void bloomfilter_shm_free(void *filter, size_t);
+
+bloomfilter_swap_t *bloomfilterswap_new(bloomfilter_allocator allocator);
+
+void bloomfilterswap_destroy(bloomfilter_swap_t **swap,
+			     bloomfilter_deallocator deallocator);
+
+void bloomfilterswap_swap(bloomfilter_swap_t *filter);
+
+void bloomfilterswap_add(bloomfilter_swap_t *filter, const void *key,
+			 size_t keylen);
+
+int bloomfilterswap_test(bloomfilter_swap_t *filter, const void *key,
+			 size_t keylen);
+
+]]
+
+local C = ffi.load("../build/libbloomfilter.so")
+
+local bloomfilter_swap_t = ffi.typeof("bloomfilter_swap_t *[1]")
+
+local string_check = function(filter, func, element)
+    if type(element) ~= "string" then
+        ngx.log(ngx.ERR, type(element));
+        return nil, "element must be string type"
+    end
+
+    return func(filter, element, #element) == 1
+end
+
+local bloomfilter_gc = function(filter)
+    return C.bloomfilterswap_destroy(filter, C.bloomfilter_shm_free)
+end
+
+return function()
+
+    local filter = bloomfilter_swap_t()
+    filter[0] =  C.bloomfilterswap_new(C.bloomfilter_shm_alloc)
+    if filter[0] == nil then
+        return nil, "failed to initialize filter"
+    end
+
+    --ffi.gc(filter, bloomfilter_gc)
+
+    return {
+        swap = function()
+            return C.bloomfilterswap_swap(filter[0])
+        end,
+
+        add = function(element)
+            return string_check(filter[0], C.bloomfilterswap_add, element)
+        end,
+
+        test = function(element)
+            return string_check(filter[0], C.bloomfilterswap_test, element)
+        end
+    }
+end
